f3934183e693087ff618af82e3a62b6a4afcfe9b
Joshua <joshua.odiwuor@turnkeyafrica.com>
2020-06-12 08:01:54 +0300

    Adds adds registration

--- reddit-clone-frontend/src/app/app.module.ts
+++ reddit-clone-frontend/src/app/app.module.ts
@@ -1,18 +1,28 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
+import {AppComponent} from './app.component';
+import {HeaderComponent} from './header/header.component';
+import {SignUpComponent} from './auth/signup/sign-up.component';
+import {AppRoutingModule} from './app-routing.module';
+import {ReactiveFormsModule} from '@angular/forms';
+import {AuthModule} from './auth/auth.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    SignUpComponent
   ],
   imports: [
-    BrowserModule
+    AppRoutingModule,
+    BrowserModule,
+
+    ReactiveFormsModule,
+    AuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
---
+++ reddit-clone-frontend/src/app/auth/signup/sign-up.component.ts
@@ -0,0 +1,42 @@
+import {Component, OnInit} from '@angular/core';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
+import {SignUpRequestPayload} from '../../models/signup-request.payload';
+import {AuthService} from "../shared/auth.service";
+
+@Component({
+  selector: 'app-signup',
+  templateUrl: './sign-up.component.html',
+  styleUrls: ['./sign-up.component.css']
+})
+export class SignUpComponent implements OnInit {
+  signUpRequestPayload: SignUpRequestPayload;
+  signUpForm: FormGroup;
+
+  constructor(private  authService: AuthService) {
+    this.signUpRequestPayload = {
+      username: '',
+      email: '',
+      password: ''
+    };
+  }
+
+  ngOnInit(): void {
+    this.signUpForm = new FormGroup({
+      username: new FormControl(null, Validators.required),
+      email: new FormControl(null, [Validators.required, Validators.email]),
+      password: new FormControl(null, Validators.required)
+    })
+    ;
+  }
+
+  signUp() {
+    this.signUpRequestPayload.username = this.signUpForm.get('username').value;
+    this.signUpRequestPayload.email = this.signUpForm.get('email').value;
+    this.signUpRequestPayload.password = this.signUpForm.get('password').value;
+    this.authService.signUp(this.signUpRequestPayload).subscribe(() => {
+      console.log('SignUp Successful');
+    }, () => {
+      console.log('SignUp Failed');
+    });
+  }
+}
